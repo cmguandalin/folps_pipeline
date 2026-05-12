eac76b4ef4c7109fd2bb319f472473c38dd468a1
Caroline Macedo Guandalin <caroline.guandalin@roe.ac.uk>
2026-05-12 10:54:31 +0100

    v3
    Includes window function for the bispectrum;

--- src/inference.py
+++ src/inference.py
@@ -1,7 +1,7 @@
 import numpy as np
 import argparse
 import os,sys
-import glob, re
+import glob
 import yaml
 from time import time
 import pocomc as pc
@@ -11,7 +11,6 @@ import likelihood as clike
 import model as model
 from datetime import datetime
 import multiprocess as mp
-
 ctx = mp.get_context('fork')
 
 #os.environ['TF_NUM_INTRAOP_THREADS'] = '1'
@@ -63,23 +62,25 @@ if __name__ == '__main__':
         cov_path = data_path+cov_file
     # Number of mocks for hartlap correction
     number_of_mocks = config['number_of_mocks']
-    # Rescaling factor for the covariance
+    # Rescaling factor for the covariance (e.g., rescale = 0.25 => Cov = Cov*0.25)
     rescale = config['rescale']
     # Minimum and maximum wavenumbers to consider
     k_edges = config['k_edges']
+    # Multipoles for the analysis
+    multipoles = list(data_files.keys())
+    
     # The priors
-    priors = config['prior']
+    priors        = config['prior']
     reparametrize = config['reparametrize']
-    # The files
+    # Saving options
     path_to_save = config['path_to_save']
-    file_name = config['file_name']
-
-    # For the emulator
-    multipoles   = list(data_files.keys())
+    file_name    = config['file_name']
+    # Sample related things
     mean_density = config['mean_density']
     redshift     = config['redshift']
+    
+    # Removed the emulator option. Only folps now.
     backend      = config['backend']
-
     if backend == 'emulator':
         cache_path = config['cache_path']
     else:
@@ -91,12 +92,7 @@ if __name__ == '__main__':
     #######################
     # CLEANING PARAMETERS #
     #######################
-
-    # # Iterate over a copy of the dictionary to avoid modifying it while iterating
-    # for param, prior_info in list(priors.items()):
-    #    if prior_info['type'] == 'Fix':
-    #        del priors[param]
-
+    # Iterate over a copy of the dictionary to avoid modifying it while iterating
     parameters_to_be_varied = priors.copy()
     for param, prior_info in list(priors.items()):
         if prior_info['type'] == 'Fix':
@@ -119,21 +115,20 @@ if __name__ == '__main__':
 
     # Apply Hartlap correction factor and invert covariance
     hartlap = (number_of_mocks - len(full_data) - 2) / (number_of_mocks - 1)
+    print(f'hartlap={hartlap}')
     inv_cov = hartlap * np.linalg.inv(covariance)
 
-    ########################
-    # LOAD WINDOW FUNCTION #
-    ########################
+    #########################
+    # LOAD WINDOW FUNCTIONS #
+    #########################
+    if 'window_file_P' in config:
+        wcmat_P = np.load(config['window_file_P'],allow_pickle=True).item()
 
-    if 'window_file' in config:
-        # Load window matrix once
-        data = np.load(config['window_file'],allow_pickle=True).item()
-
-        value    = data['value']
-        xin      = data['xin']
-        xout     = data['xout']
-        ells_in  = np.array([proj['ell'] for proj in data['projsin']])
-        ells_out = np.array([proj['ell'] for proj in data['projsout']])
+        value    = wcmat_P['value']
+        xin      = wcmat_P['xin']
+        xout     = wcmat_P['xout']
+        ells_in  = np.array([proj['ell'] for proj in wcmat_P['projsin']])
+        ells_out = np.array([proj['ell'] for proj in wcmat_P['projsout']])
 
         xin_flat  = np.array(xin).flatten()
         xout_flat = np.array(xout).flatten()
@@ -155,6 +150,9 @@ if __name__ == '__main__':
         value     = value[:, mask_out]
 
         # OUT multipole selection
+        multipoles_for_convolution = {}
+        multipoles_for_convolution['Pk'] = [str(ell_) for ell_ in ells_in]
+
         print(f'Selecting multipoles for analysis: {multipoles}')
         Nout_orig = 3
         Nin_total = value.shape[0]
@@ -164,28 +162,49 @@ if __name__ == '__main__':
         value_reshaped_selected = value_reshaped[:, mask_ell_out, :]
         value = value_reshaped_selected.reshape(Nin_total, -1)
 
-        k_theory_window = xin_flat.reshape(np.array(xin).shape[0], int(len(xin_flat)/Nout_orig))[0]
-        #k_obs_window    = np.array(xout_flat)
-        wvalue          = value.T
-
-        multipoles_for_convolution = [str(ell_) for ell_ in ells_in]
-
+        k_theory_window = {}
+        k_theory_window['Pk'] = xin_flat.reshape(np.array(xin).shape[0], int(len(xin_flat)/Nout_orig))[0]
+        window_matrix       = {}
+        window_matrix['Pk'] = value.T
     else:
-        wvalue = None
+        window_matrix = None
+
+    # BISPECTRUM WINDOW MATRIX
+    import h5py
+    if 'window_file_B' in config:
+        # GCcombined window multipoles for convolution
+        window_file_B = config['window_file_B']
+
+        if 'window_matrix' not in locals():
+            window_matrix = {}
+        if 'k_theory_window' not in locals():
+            k_theory_window = {}
+        if 'multipoles_for_convolution' not in locals():
+            multipoles_for_convolution = {}
+
+        window_matrix['Bk'] = {}
+        multipoles_for_convolution['Bk'] = {}
+        for L in multipoles:
+            if len(L)==3:
+                with h5py.File(window_file_B[L], 'r') as f:
+                    window_matrix['Bk'][L] = f['wcmat'][:]
+                    k_theory_window['Bk'] = f['k_input'][:]
+                    l1l2L_input = f['l1l2L_input'][:]
+                    multipoles_for_convolution['Bk'][L] = [l1l2L.decode('utf-8') for l1l2L in l1l2L_input]
+        f.close()
 
     ################
     # MODEL VECTOR #
     ################
     # Initialise emulators
-    if wvalue is not None:
+    if window_matrix is not None:
         print('Convolving with window')
         # Change the redshift to the effective one from the window
-        redshift = data['attrs']['zeff']
+        redshift = wcmat_P['attrs']['zeff']
         print(f'Updating redshift to the window effective z={redshift}.')
 
         if backend == 'folps':
             calculator = model.FOLPSCalculator(
-                multipoles_for_convolution,
                 mean_density,
                 redshift,
                 model=theory_model,
@@ -211,13 +230,12 @@ if __name__ == '__main__':
                                 data,
                                 calculator,
                                 multipoles_for_convolution,
-                                window_matrix=wvalue,
+                                window_matrix=window_matrix,
                                 k_theory_window=k_theory_window
                             )
     else:
         if backend == 'folps':
             calculator = model.FOLPSCalculator(
-                multipoles,
                 mean_density,
                 redshift,
                 model=theory_model,
@@ -246,6 +264,8 @@ if __name__ == '__main__':
     likelihood = clike.Likelihood(priors, model_function)#, debug_filename="/Users/austerlitz/folps/pipeline/test_debug_2.txt")
     prior = likelihood.initialise_prior()
 
+    # Likelihood would be vectorised
+
     # Assign to global variables
     global_full_data  = full_data
     global_inv_cov    = inv_cov
@@ -267,8 +287,9 @@ if __name__ == '__main__':
 
     print(f'Starting sampling at {datetime.now()} with {ncpus} CPUs. \n')
 
+    '''
+    ########################
     # Check for checkpoints
-
     final_state = os.path.join(path_to_save, f"{file_name}_final.state")
 
     if os.path.exists(final_state):
@@ -286,9 +307,13 @@ if __name__ == '__main__':
     if state_files:
         resume_file = state_files[-1]
         print(f"Resuming from {resume_file}")
+    #
+    ###################
+    '''
 
     if ncpus > 1:
         with ctx.Pool(ncpus) as pool:
+        #with mp.Pool(ncpus) as pool:
             sampler = pc.Sampler(
                 prior=prior,
                 likelihood=likelihood_wrapper,
@@ -297,7 +322,7 @@ if __name__ == '__main__':
                 output_dir=path_to_save,
                 output_label=file_name
             )
-            sampler.run(n_total=ntot, progress=True, save_every=25, resume_state_path=resume_file)
+            sampler.run(n_total=ntot, progress=True, save_every=200)
 
     else:
         sampler = pc.Sampler(
@@ -307,7 +332,7 @@ if __name__ == '__main__':
             output_dir=path_to_save,
             output_label=file_name
         )
-        sampler.run(n_total=ntot, progress=True, save_every=25, resume_state_path=resume_file)
+        sampler.run(n_total=ntot, progress=True, save_every=200)
 
     samples, weights, logl, logp = sampler.posterior()
 
